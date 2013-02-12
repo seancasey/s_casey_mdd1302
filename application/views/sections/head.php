1b496cd0b9e35c2437b3ed098dfcfa562f6bf1ce
Sean <seancasey08@gmail.com>
2013-02-12 01:34:42 -0500

    Added Jquery Mobile

--- application/views/sections/head.php
+++ application/views/sections/head.php
@@ -39,6 +39,7 @@
 	<!-- Included CSS Files -->
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/foundation'); ?>/stylesheets/foundation.css">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/foundation'); ?>/stylesheets/app.css">
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.css" />
 	
 
 	<!-- My Stylesheet -->
@@ -50,6 +51,11 @@
 	<![endif]-->
 
 	<script src="<?php echo base_url('assets/foundation'); ?>/javascripts/modernizr.foundation.js"></script>
+	
+	<!--Jquery Mobile Scripts-->
+	<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
+	<script src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script>
+	<!-- End Jquery Mobile Scripts -->
 
 	<!-- IE Fix for HTML5 Tags -->
 	<!--[if lt IE 9]>
