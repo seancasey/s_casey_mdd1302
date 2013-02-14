aa373a39625536951fd4797ea7912345569c74a5
Sean <seancasey08@gmail.com>
2013-02-14 04:24:29 -0500

    Update on Views

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -5,18 +5,42 @@
 			<div class="twelve columns centered center-image">
 				<img id="logo" src="<?php echo base_url('assets/images/logo.png')?>" />
 			</div>
+			
+			<div class="row">
+				<div class="eight columns centered">
+          	    	<div class="panel callout radius intro-panel">
+					    <h3>Welcome to Challenge Accepted!</h3>
+					    <p>Members may sign in below.  If you are new to this app you can either sign in with Facebook or click the register button below.</p>
+				    </div>
+				</div>
+		   <div>
+		
+				 
+			
 			<form action="form.php" method="post">
-				<div data-role="fieldcontain" class="ui-hide-label">
+				<div data-role="fieldcontain" class="ui-hide-label center-image">
 					<label for="username">Email:</label>
 					<input type="text" name="username" id="username" value="" placeholder="Username"/>
 				</div>
-				<div data-role="fieldcontain" class="ui-hide-label">
+				<div data-role="fieldcontain" class="ui-hide-label center-image">
 					<label for="username">Password:</label>
 					<input type="text" name="username" id="username" value="" placeholder="Password"/>
 				</div>
-				<input type="button" class="small button radius" value="Login" />
+				<div class="row">
+					<div class="six columns centered">
+						<div data-role="fieldcontain" class="ui-hide-label">
+							<input type="button" class="small button radius " value="Login" />
+						</div>
+					</div>
+				</div>
 			</form>
-			<div class="small button radius">Register</div>
+			<hr />
+			<div class="row">
+				<div class="six columns centered">
+					<button data-theme="b">Sign in with Facebook</button>
+					<div class="small button radius">Register</div>
+				</div>
+			</div>
 		</div>
 	</div>
 
