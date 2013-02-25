30f61f1c4d03e7dbee66783d17aff50f0c819ec2
Sean Casey <seancasey08@gmail.com>
2013-02-25 18:27:18 -0500

    added a jquery mobile template and css

--- application/views/new_challenge.php
+++ application/views/new_challenge.php
@@ -3,15 +3,18 @@
 	<head> 
 		<title>Page Title</title> 
 		
-		<meta name="viewport" content="width=device-width, initial-scale=1"> 
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:300italic,400italic,700italic,400,300,700|Roboto:400,700,500,400italic,500italic&subset=latin,latin-ext' rel='stylesheet' type='text/css'> 
 		<link rel="stylesheet" href="<?php echo base_url(); ?>/css/style.css" />
+		
 		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.css" />
 		<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
 		<script src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script>
+		<link rel="stylesheet" href="<?php echo base_url(); ?>/css/jquery_mobile/theme_d.css" />
 	</head>
 	<body> <!-- first page is the view that you will see when you log in -->
-		<div data-role="page" data-theme="a" id="home">
-			<div data-role="header">
+		<div data-role="page" data-theme="d" id="home">
+			<div data-role="header" data-theme="d">
 				<h1 id="header">Home</h1>
 				<a class="logout" href="<?php echo base_url('index.php/user')?>" data-icon="delete" class="ui-btn-left" data-transition="slidefade">Home</a>
 				<a class="logout" href="<?php echo base_url('index.php/user/logout')?>" data-icon="delete" class="ui-btn-right" data-transition="slidefade">Logout</a>
